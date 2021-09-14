4b57e8c743f0ff5598129a0f2c67b53d8f7f8076
VeronikaMonarkhovich <taory4cources@gmail.com>
2021-09-14 19:58:20 +0300

    demoqa-tests

--- src/test/java/com/taory/tests/TestData.java
+++ src/test/java/com/taory/tests/TestData.java
@@ -1,12 +1,11 @@
 package com.taory.tests;
 
-import com.github.javafaker.DateAndTime;
 import com.github.javafaker.Faker;
-
 import java.util.Locale;
 
 public class TestData {
         static Faker faker = new Faker(new Locale("en"));
+
         public static String firstName = faker.name().firstName();
         public static String lastName = faker.name().lastName();
         public static String userEmail = faker.internet().emailAddress();
