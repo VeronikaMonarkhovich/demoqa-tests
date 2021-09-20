3d1187fc53a10d3a8f0dc19a10df4dbb161c352a
VeronikaMonarkhovich <taory4cources@gmail.com>
2021-09-20 21:59:04 +0300

    fix tests

--- src/test/java/com/taory/pages/PracticeFormPage.java
+++ src/test/java/com/taory/pages/PracticeFormPage.java
@@ -2,28 +2,31 @@ package com.taory.pages;
 
 import com.codeborne.selenide.SelenideElement;
 import com.taory.pages.components.CalendarComponent;
+
+import static com.codeborne.selenide.Condition.pseudo;
 import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.withText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class PracticeFormPage {
 
-   private SelenideElement formTitle =  $(".practice-form-wrapper"),
-                           firstNameInput = $("#firstName"),
-                           lastNameInput = $("#lastName"),
-                           userEmailInput = $("#userEmail"),
-                           userNumberInput = $("#userNumber"),
-                           currentAddressInput = $("#currentAddress");
+    private SelenideElement formTitle = $(".practice-form-wrapper"),
+            firstNameInput = $("#firstName"),
+            lastNameInput = $("#lastName"),
+            userEmailInput = $("#userEmail"),
+            userNumberInput = $("#userNumber"),
+            currentAddressInput = $("#currentAddress");
 
-   private final String FORM_TITLE = "Student Registration Form";
+    private final String FORM_TITLE = "Student Registration Form";
 
-   public CalendarComponent calendar = new CalendarComponent();
+    public CalendarComponent calendar = new CalendarComponent();
 
-    public void openPage(){
+    public void openPage() {
         open("https://demoqa.com/automation-practice-form");
         formTitle.shouldHave(text(FORM_TITLE));
     }
 
-    public void typeFirstName(String value){
+    public void typeFirstName(String value) {
         firstNameInput.setValue(value);
     }
 
@@ -31,20 +34,52 @@ public class PracticeFormPage {
         lastNameInput.setValue(value);
     }
 
-    public void typeUserEmail(String value){
+    public void typeUserEmail(String value) {
         userEmailInput.setValue(value);
     }
 
-    public void typeUserNumber(String value){
+    public void selectGender(String value) {
+        $(String.format("input[value = %s]", value)).sibling(0).click();
+    }
+
+    public void typeSubject(String value) {
+        $("#subjectsContainer").find("input").setValue(value).pressEnter();
+    }
+
+    public void selectHobbies(String value) {
+        $x(String.format("//*[contains(@for, 'hobbies-checkbox') and .='%s']", value)).click();
+    }
+
+    public void uploadFile(String value) {
+        $("#uploadPicture").uploadFromClasspath(value);
+    }
+
+    public void typeUserNumber(String value) {
         userNumberInput.setValue(value);
     }
 
-    public void typeCurrentAddress(String value){
+    public void typeCurrentAddress(String value) {
         currentAddressInput.setValue(value);
     }
 
-    public void checkResultsValue(String key,String value){
-        $x("//td[text()='"+ key +"']/following-sibling::td").shouldHave(text(value));
+    public void selectState(String value) {
+        $("#state").find("input").setValue(value).pressEnter();
+    }
+
+    public void selectCity(String value) {
+        $("#city").find("input").setValue(value).pressEnter();
+    }
+
+    public void clickSubmit() {
+        $("#submit").scrollTo().click();
+    }
+
+    public void checkResultsText(String value) {
+        $(".modal-content").shouldHave(text(value));
+    }
+
+    public void checkResultsValue(String key, String value) {
+        $x("//td[text()='" + key + "']/following-sibling::td").shouldHave(text(value));
     }
 
 }
--- src/test/java/com/taory/pages/tests/PracticeFormTest.java
+++ src/test/java/com/taory/pages/tests/PracticeFormTest.java
@@ -23,6 +23,10 @@ public class PracticeFormTest {
     String day = "15";
     String month = "August";
     String year = "1990";
+    String gender = "Male";
+    String hobbie = "Reading";
+    String file = "test.png";
+    String text = "Thanks for submitting the form";
 
     PracticeFormPage practiceFormPage = new PracticeFormPage();
 
@@ -32,28 +36,28 @@ public class PracticeFormTest {
         practiceFormPage.typeFirstName(firstName);
         practiceFormPage.typeLastName(lastName);
         practiceFormPage.typeUserEmail(userEmail);
-        $("[for=gender-radio-1]").click();
+        practiceFormPage.selectGender(gender);
         practiceFormPage.typeUserNumber(userNumber);
-        practiceFormPage.calendar.setDate(day,month,year);
-        $("#subjectsContainer").find("input").setValue(subjects).pressEnter();
-        $("[for=hobbies-checkbox-2]").click();
-        $("#uploadPicture").uploadFromClasspath("test.png");
+        practiceFormPage.calendar.setDate(day, month, year);
+        practiceFormPage.typeSubject(subjects);
+        practiceFormPage.selectHobbies(hobbie);
+        practiceFormPage.uploadFile(file);
         practiceFormPage.typeCurrentAddress(currentAddress);
-        $("#state").find("input").setValue(state).pressEnter();
-        $("#city").find("input").setValue(city).pressEnter();
-        $("#submit").scrollTo().click();
+        practiceFormPage.selectState(state);
+        practiceFormPage.selectCity(city);
+        practiceFormPage.clickSubmit();
 
-        $(".modal-content").shouldHave(text("Thanks for submitting the form"));
-        practiceFormPage.checkResultsValue("Student Name",firstName + " " + lastName);
-        practiceFormPage.checkResultsValue("Student Email",userEmail);
-        practiceFormPage.checkResultsValue("Gender","Male");
-        practiceFormPage.checkResultsValue("Mobile",userNumber);
-        practiceFormPage.checkResultsValue("Date of Birth",day + " " + month + "," + year);
-        practiceFormPage.checkResultsValue("Subjects",subjects);
-        practiceFormPage.checkResultsValue("Hobbies","Reading");
-        practiceFormPage.checkResultsValue("Picture","test.png");
-        practiceFormPage.checkResultsValue("Address",currentAddress);
-        practiceFormPage.checkResultsValue("State and City",state + " " + city);
+        practiceFormPage.checkResultsText(text );
+        practiceFormPage.checkResultsValue("Student Name", firstName + " " + lastName);
+        practiceFormPage.checkResultsValue("Student Email", userEmail);
+        practiceFormPage.checkResultsValue("Gender", gender);
+        practiceFormPage.checkResultsValue("Mobile", userNumber);
+        practiceFormPage.checkResultsValue("Date of Birth", day + " " + month + "," + year);
+        practiceFormPage.checkResultsValue("Subjects", subjects);
+        practiceFormPage.checkResultsValue("Hobbies", hobbie);
+        practiceFormPage.checkResultsValue("Picture", file);
+        practiceFormPage.checkResultsValue("Address", currentAddress);
+        practiceFormPage.checkResultsValue("State and City", state + " " + city);
 
     }
 }
